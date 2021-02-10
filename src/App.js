23d105fc25a364137945c73a9c52272266add5fc
Md Maruf Ahmed <maruf.ahmed76@yahoo.com>
2021-02-10 15:49:35 +0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen width-screen flex flex-row justify-center items-center">
+      <h1 className="text-5xl font-medium">My React and tailwind Boilerplate</h1>
+    </div>
+  );
+}
+
+export default App;
