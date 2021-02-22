364011b8b0a313fd34bd0375460ae625b42084c6
Md Maruf Ahmed <maruf.ahmed76@yahoo.com>
2021-02-22 20:33:04 +0600

    Working sorting steps

--- src/App.js
+++ src/App.js
@@ -37,24 +37,27 @@ function App() {
   };
 
   const doAnimation = (sortedArr) => {
+    let domBar = document.getElementsByClassName('item-bar');
     for (let i = 0; i < sortedArr.length; i++) {
-      const domBar = document.getElementsByClassName('item-bar');
-      const isColor = i % 3 !== 2;
-      if (isColor) {
-        const [firstBar, secondBar] = sortedArr[i];
-        const firstBarStyle = domBar[firstBar].style;
-        const secondBarStyle = domBar[secondBar].style;
-        const color = i % 3 === 0 ? 'red' : 'rgba(16, 185, 129, var(--tw-bg-opacity))';
+      let isColor = i % 3 !== 0;
+      if (!isColor) {
+        let [firstBar, secondBar] = sortedArr[i];
+        let firstBarStyle = domBar[firstBar].style;
+        let secondBarStyle = domBar[secondBar].style;
+        // let color = i % 3 !== 0 ? 'red' : 'rgba(16, 185, 129, var(--tw-bg-opacity))';
+        let color = 'red';
         setTimeout(() => {
           firstBarStyle.backgroundColor = color;
           secondBarStyle.backgroundColor = color;
-        }, i * 5);
+        }, i * 0);
       } else {
         setTimeout(() => {
-          const [selectedBar, selectedBarHeight] = sortedArr[i];
-          const selectedBarStyle = domBar[selectedBar].style;
-          selectedBarStyle.height = `${selectedBarHeight}%`;
-        }, i * 5);
+          let [selectedBar, selectedBarHeight] = sortedArr[i];
+          let selectedBarStyle = domBar[selectedBar].style;
+          selectedBarStyle.height = `${selectedBarHeight / 100}%`;
+          let color = i % 3 !== 1 ? 'red' : 'rgba(16, 185, 129, var(--tw-bg-opacity))';
+          selectedBarStyle.backgroundColor = color;
+        }, i * 0);
       }
     }
   };
--- src/algorithms/BubbleSort.js
+++ src/algorithms/BubbleSort.js
@@ -26,11 +26,11 @@ function* BubbleSortSteps(arr = []) {
   for (let i = newArr.length - 1; i >= 0; i--) {
     for (let j = 0; j < i; j++) {
       yield [j, j + 1];
-      yield [j, j + 1];
       if (newArr[j] > newArr[j + 1]) {
         [newArr[j], newArr[j + 1]] = [newArr[j + 1], newArr[j]];
       }
       yield [j, newArr[j]];
+      yield [j + 1, newArr[j + 1]];
     }
   }
 }
