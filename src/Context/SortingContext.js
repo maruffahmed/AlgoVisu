8529a6264f0bbe6534168f90d5b1c93057a3832d
Md Maruf Ahmed <maruf.ahmed76@yahoo.com>
2021-02-24 13:10:09 +0600

    Remove initial array value from sortingContext

--- src/Context/SortingContext.js
+++ src/Context/SortingContext.js
@@ -1,39 +1,39 @@
 import React from 'react';
 import { BubbleSortSteps } from 'algorithms';
-import { initialValue, randomNum } from 'algorithms/lib';
+import { randomNum } from 'algorithms/lib';
 import { BubbleSortAnimation } from 'algorithmAnimation';
 
 const SortingContext = React.createContext();
 SortingContext.displayName = 'SortingContext';
 
 function SortingContextProvider(props) {
-  const [initialArray, setInitialArray] = React.useState(initialValue); // Unsorted initial array
   // Make a random array
-  const randomArr = () => {
+  const randomArr = (setNewArr) => {
     let newArr = [];
     for (let i = 0; i < 100; i++) {
       newArr.push(randomNum(500, 10000));
     }
-    setInitialArray(newArr);
+    setNewArr(newArr);
   };
   // Buttble sort action
-  const doBubbleSort = () => {
+  const doBubbleSort = (theArr = []) => {
     // BubbleSort();
     let bubbleSortSteps = []; // Bubble sort steps array
-    for (let step of BubbleSortSteps(initialArray)) {
+    for (let step of BubbleSortSteps(theArr)) {
       bubbleSortSteps.push(step);
     }
+    console.log(bubbleSortSteps);
     // console.log(bubbleSortSteps);
     BubbleSortAnimation(bubbleSortSteps);
   };
 
   // Js buildin sort()
-  const doSort = () => {
-    const newArr = [...initialArray];
+  const doSort = (theArr = [], setNewArr) => {
+    const newArr = [...theArr];
     newArr.sort((a, b) => a - b);
-    setInitialArray(newArr);
+    setNewArr(newArr);
   };
-  const value = { initialArray, randomArr, doBubbleSort, doSort };
+  const value = { randomArr, doBubbleSort, doSort };
   return <SortingContext.Provider value={value} {...props} />;
 }
 
