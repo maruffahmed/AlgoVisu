e926b0ff7a468c0b2f88aaaf33ef41ff3f3f2704
Md Maruf Ahmed <maruf.ahmed76@yahoo.com>
2021-02-27 20:21:46 +0600

    Add comming soon page

---
+++ src/components/CommingSoon.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function CommingSoon(props) {
+  return (
+    <div className="flex justify-center items-center h-full">
+      <h1 className="text-4xl text-gray-700 italic">Coming Soon</h1>
+    </div>
+  );
+}
+
+export default CommingSoon;
--- src/components/Insertion/Insertion.js
+++ src/components/Insertion/Insertion.js
@@ -1,11 +1,8 @@
 import React from 'react';
+import CommingSoon from 'components/CommingSoon';
 
 function Insertion(props) {
-  return (
-    <div className="flex justify-center items-center h-full">
-      <h1 className="text-4xl text-gray-700 italic">Coming Soon</h1>
-    </div>
-  );
+  return <CommingSoon />;
 }
 
 export default Insertion;
