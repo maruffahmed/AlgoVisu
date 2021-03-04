9b3854c695f9de2ff2ec6e42900b44f17d63ac46
Md Maruf Ahmed <maruf.ahmed76@yahoo.com>
2021-03-05 01:01:23 +0600

    Change default 25 bars to 50 bars fro Quick sort context

--- src/Context/QuickSortContext.js
+++ src/Context/QuickSortContext.js
@@ -8,7 +8,7 @@ QuickSortContext.displayName = 'QuickSortContext';
 
 function QuickSortContextProvider(props) {
   const [quickArray, setQuickArray] = React.useState(null);
-  const [quickBars, setQuickBars] = React.useState(25);
+  const [quickBars, setQuickBars] = React.useState(50);
   const { animationSpeed, setStartAnimation, setTotalCountedSteps } = useSortingContext();
 
   // Buttble sort action
