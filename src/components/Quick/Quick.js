20cc925ef26ff472bba79c35aabcc9d0286a75bb
Md Maruf Ahmed <maruf.ahmed76@yahoo.com>
2021-03-05 00:39:29 +0600

    Chnage 100 bar to 70 bar in Quick Sort page

--- src/components/Quick/Quick.js
+++ src/components/Quick/Quick.js
@@ -67,7 +67,7 @@ function Quick() {
               <option value="15">15</option>
               <option value="25">25</option>
               <option value="50">50</option>
-              <option value="100">100</option>
+              <option value="70">70</option>
             </select>
           </div>
         </div>
