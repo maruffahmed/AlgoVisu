bea8eb3a2057e468cc15b021c8093328e3367396
Md Maruf Ahmed <maruf.ahmed76@yahoo.com>
2021-03-08 23:57:23 +0600

    initial add of merge sort animation

--- src/algorithmAnimation/index.js
+++ src/algorithmAnimation/index.js
@@ -1,5 +1,6 @@
 import { BubbleSortAnimation } from './BubbleSortAnimation';
 import { InsertionSortAnimation } from './InsertionSortAnimation';
 import { QuickSortAnimation } from './QuickSortAnimation';
+import { MergeSortAnimation } from './MergeSortAnimation';
 
-export { BubbleSortAnimation, InsertionSortAnimation, QuickSortAnimation };
+export { BubbleSortAnimation, InsertionSortAnimation, QuickSortAnimation, MergeSortAnimation };
