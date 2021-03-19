3f9bd5900c7a5091a5468290374a7428a641e795
Md Maruf Ahmed <maruf.ahmed76@yahoo.com>
2021-03-19 22:10:45 +0600

    Fixed merge sort animation

--- src/algorithmAnimation/MergeSortAnimation.js
+++ src/algorithmAnimation/MergeSortAnimation.js
@@ -2,49 +2,25 @@ export const MergeSortAnimation = (sortedArr = [], speed = 10, setTotalCountedSt
   let domBar = document.getElementsByClassName('item-bar');
   // let it = 1;
   for (let i = 0; i < sortedArr.length; i++) {
-    let needToPlace = sortedArr[i].length > 1;
-    if (!needToPlace) {
-      if (sortedArr[i].hasOwnProperty('i')) {
-        let [theBar] = sortedArr[i].i;
-        let selectTheBar = domBar[theBar];
-        // let color = i % 3 !== 0 ? 'red' : 'rgba(16, 185, 129, var(--tw-bg-opacity))';
-        let color = 'rgba(245, 158, 11, var(--tw-bg-opacity))';
-        setTimeout(() => {
-          selectTheBar.style.backgroundColor = color;
-          setTotalCountedSteps((prevCount) => prevCount + 1);
-        }, i * speed);
-      } else {
-        let [theBar] = sortedArr[i].j;
-        let selectTheBar = domBar[theBar];
-        // let color = i % 3 !== 0 ? 'red' : 'rgba(16, 185, 129, var(--tw-bg-opacity))';
-        let color = 'rgba(245, 158, 11, var(--tw-bg-opacity))';
-        setTimeout(() => {
-          selectTheBar.style.backgroundColor = color;
-          setTotalCountedSteps((prevCount) => prevCount + 1);
-        }, i * speed);
-      }
-    } else {
-      // let needToColor = it % 3 === 1;
+    let isColor = i % 3 !== 2;
+
+    if (isColor) {
+      let [firstBar, secondBar] = sortedArr[i];
+      let selectFirstBar = domBar[firstBar];
+      let selectSecondtBar = domBar[secondBar];
+      let color = i % 3 === 0 ? 'red' : 'rgba(16, 185, 129, var(--tw-bg-opacity))';
+      // let color = 'rgba(245, 158, 11, var(--tw-bg-opacity))';
       setTimeout(() => {
-        // if (needToColor) {
-        //   let [firstBar, secondBar] = sortedArr[i];
-        //   let selectFirstBar = domBar[firstBar];
-        //   let selectSecondtBar = domBar[secondBar];
-        //   selectFirstBar.style.backgroundColor = 'red';
-        //   selectSecondtBar.style.backgroundColor = 'red';
-        // } else {
-        // }
-        if (sortedArr[i].hasOwnProperty('i')) {
-          let [{ i: theBar }, theHeight] = sortedArr[i];
-          let selectTheBar = domBar[theBar];
-          selectTheBar.style.height = `${theHeight / 100}%`;
-          selectTheBar.style.backgroundColor = 'rgba(16, 185, 129, var(--tw-bg-opacity))';
-        } else {
-          let [{ j: theBar }, theHeight] = sortedArr[i];
-          let selectTheBar = domBar[theBar];
-          selectTheBar.style.height = `${theHeight / 100}%`;
-          selectTheBar.style.backgroundColor = 'rgba(16, 185, 129, var(--tw-bg-opacity))';
-        }
+        selectFirstBar.style.backgroundColor = color;
+        selectSecondtBar.style.backgroundColor = color;
+        setTotalCountedSteps((prevCount) => prevCount + 1);
+      }, i * speed);
+    } else {
+      setTimeout(() => {
+        let [theBar, theHeight] = sortedArr[i];
+        let selectTheBar = domBar[theBar];
+        selectTheBar.style.height = `${theHeight / 100}%`;
+        // selectTheBar.style.backgroundColor = 'rgba(16, 185, 129, var(--tw-bg-opacity))';
       }, i * speed);
       // it++;
       setTotalCountedSteps((prevCount) => prevCount + 1);
