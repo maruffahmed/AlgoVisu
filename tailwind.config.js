8ad2901763427fdbffb7543aaaad68df078d5605
Md Maruf Ahmed <maruf.ahmed76@yahoo.com>
2021-03-20 00:15:30 +0600

    Extend body font familly

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        body: ['Open Sans'],
+      },
+    },
   },
   variants: {
     extend: {},
